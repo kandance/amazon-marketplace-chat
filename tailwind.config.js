0fb198ed88bca094c7c031bd544244bf312415af
Kandance Ferguson <kandance.ferguson@gmail.com>
2023-03-05 23:03:31 -0500

    setup tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontSize: {
+      tiny: '0.7rem',
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {
+      display: ['group-hover'],
+    },
+  },
+  plugins: [],
+};
